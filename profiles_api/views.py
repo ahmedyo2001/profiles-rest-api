2896bfcb5ded299c980884275a3465010caac153
ahmedyo2001@gmail.com <MY_NAME@example.com>
2022-08-10 23:19:41 +0200

    fast

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+
+class HelloApiView (APIView):
+
+    def get(self, request, format=None):
+        an_apiview=[
+            "uses http methods",
+            "Is similar to a trad django view",
+            'gives u most control to app logic',
+        ]
+
+        return Response({'message':'Hello','an_apiview':an_apiview})
+
